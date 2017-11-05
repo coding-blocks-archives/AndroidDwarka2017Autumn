e5977d18812090ad1068a4fe64b5dbf7558d0303
the-dagger <harshithdwivedi@gmail.com>
2017-11-05 14:07:03 +0530

    add code for firebase auth
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

--- Lecture16/Firebase/app/src/main/java/com/codingblocks/firebase/MainActivity.java
+++ Lecture16/Firebase/app/src/main/java/com/codingblocks/firebase/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codingblocks.firebase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,6 +19,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                DatabaseReference newRef = rootNode.child("todo").push(); For generating random keys
 
-                Task currentTask = new Task(System.currentTimeMillis(),"Title","Description");
+                Task currentTask = new Task(System.currentTimeMillis(), "Title", "Description");
 
                 rootNode.child("task").child(String.valueOf(currentTask.getId())).setValue(currentTask);
 
@@ -74,9 +83,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Iterable<DataSnapshot> dataSnapshots = dataSnapshot.getChildren();
-                for (DataSnapshot ds : dataSnapshots){
+                for (DataSnapshot ds : dataSnapshots) {
                     Task t = ds.getValue(Task.class);
-                    Log.e("TAG", "onDataChange: " + t.getId() );
+                    Log.e("TAG", "onDataChange: " + t.getId());
                 }
             }
 
@@ -88,4 +97,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseUser fbUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (fbUser == null) {
+            //Sign in
+            Intent signInIntent = AuthUI.getInstance().createSignInIntentBuilder().build();
+            startActivity(signInIntent);
+        }
+
+        //Get details from the user
+        fbUser.getDisplayName();
+    }
 }
